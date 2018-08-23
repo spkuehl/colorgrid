304d567952f60e89ea579713626a01829b1a93b8
spkuehl <skuehlhorn@gmail.com>
2018-08-22 21:50:06 -0500

    Set tr.random-color-cells with a jquery script.

--- color_grid/static/color_grid/rotation.js
+++ color_grid/static/color_grid/rotation.js
@@ -1,11 +1,16 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
+  var counter = 0;
+  $("td.random-color-cell").each(function() {
+      $(this).attr("id", counter);
+      counter++;
+
+      // compare id to what you want
+  });
   $( "#rotate" ).click(function() {
     colorList.unshift(colorList.pop());
     console.log(colorList);
     for (i = 0; i < colorList.length; i++) {
-      console.log(i);
-      console.log(colorList[i]);
         var elem = document.getElementById(i);
         elem.style.backgroundColor = 'rgb(' + colorList[i] +')';
     }
